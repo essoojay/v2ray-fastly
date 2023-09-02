f662e9e0c4da3ea8cd3a2a96ee1397a0bb7a39d0
Facebook-App <erik.jaifer@gmail.com>
2023-09-02 02:37:40 -0500

    key in use

--- install.sh
+++ install.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
-
 global_token=""
 IP=$(curl -s https://ifconfig.io)
 
 _banner () {
-	echo -e "V2RAY FASLTY CDN\n"
+  clear
+  echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+  echo -e "            AUTH CLIENT  |  @kiredev "
+  echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 }
 
 _auth () {
-  read -p "[?] KEY: " key
-  data="_key=${key}"
-  
-  res=(`curl --silent http://api.kiredev.shop/client/auth -d "${data}" -H "X-Parse-id: 3028377422"`)
+  read -p "[~] KEY: " key
+
+  res=(`curl --silent http://api.kieredev.shop/client/auth -d "_key=$key" -H "X-Parse-id: 3028377422"`)
 
   [[ `echo -e $res | grep -E -i -w "invalid_request"` ]] && echo -e "\033[31mHay un erro en la solicitud\033[0m"
 
-  if [[ `echo -e $res | grep -E -i -w "invalid_grant"` ]]; then
+  if [[ -z $key ]]; then
+    clear
+    echo -e "\033[31mPorfavor ingrese una key\033[0m\n"
+    _auth
+  elif [[ `echo -e $res | grep -E -i -w "invalid_grant"` ]]; then
     clear
     echo -e "\033[31mSu Key es incorrecta. vuelva a intentarlo /o aquiera una nueva \033[0m\n"
+    _auth
+  elif [[ `echo -e $res | grep -E -i -w "used_key"` ]]; then
+    clear
+    echo -e "\033[31mEsta key ya fue utilizada, vuelva a intentarlo con otra. \033[0m\n"
     _auth
   fi
 
@@ -36,8 +45,7 @@ _auth () {
 }
 
 iv2ray () {
-  clear
-  echo -e "AUTH ACCOUNT"
+  clear && _banner
   echo -e "[!] ingrese su key para activar su cuenta y continuar con la instalacion\n"
   _auth
   [[ ! -f "_access.key" ]] && {
@@ -53,7 +61,6 @@ iv2ray () {
 __init () {
   #root sistem
   [[ "$(whoami)" != "root" ]] && echo -e "Se nesecita root para correr este script" && return 0
-  _banner
   iv2ray
 }
 
